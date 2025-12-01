712f9c8f36a4ca44bdd35fd66a1ef8f2530947e0
UsmanAhmadgit <usmanahmadpk21@gmail.com>
2025-12-01 23:59:33 +0500

    Remove old frontend files from root

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Login from "./pages/Auth/Login"; // Import your Login page
-
-function App() {
-  return <Login />; // Render only the Login page
-}
-
-export default App;
--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Login from "./pages/Auth/Login";
-
-function App() {
-  return <Login />; // Renders only the Login page
-}
-
-export default App;
